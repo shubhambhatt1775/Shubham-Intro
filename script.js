77170f2e2504bb553c61f99a5fb756d942b9aa99
shubhambhatt1775 <bhattshubham274@gmail.com>
2025-08-15 15:35:18 +0530

    done

--- script.js
+++ script.js
@@ -171,6 +171,24 @@ function disableContextMenu() {
 
 }
 
+var elem =document.querySelectorAll("#elem")
+
+elem.forEach(function(val){
+    console.log(val)
+
+    val.addEventListener("mouseenter",function(){
+            val.childNodes[5].style.opacity=1
+    });
+
+    val.addEventListener("mouseleave",function(){
+        val.childNodes[5].style.opacity=0
+});
+
+val.addEventListener("mousemove",function(dets){
+    val.childNodes[5].style.left=dets.x+"px"
+});
+})
+
 locomotiveScrollTriggerSetup()
 rotatingArrow()
 scrollingNamediv()
